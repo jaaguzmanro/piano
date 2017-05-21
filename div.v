ee27393651335894a211b98257ca3d46153421d8
jaaguzmanro <jaaguzmanro@unal.edu.co>
2017-05-21 16:47:27 -0500

    Update div.v

--- div.v
+++ div.v
@@ -1,13 +1,33 @@
 module div(scaler,clk_out,clk_in)
-input reg [17:0]scaler
+    input reg [2:0]scaler
 input clk_in;
-output clk_in;
+output clk_out;
 wire clk1;
-
+    wire [17:0] Do4 = 191113;
+    wire [17:0] Re4	= 170263;
+    wire [17:0] Mi4	= 151687;
+    wire [17:0] Fa4	= 143173;
+    wire [17:0] Sol4 = 127553;
+    wire [17:0] La4	= 113637;
+    wire [17:0] Si4	= 101239;
+    wire [17:0] Do5	= 95557;
+    wire [17:0] N	= 0;
+    
 always @(posedge clk_in)
-    if (divcounter == Do4 - 1) 
+    
+    case (scaler)
+        000:N=Do4;
+        001:N=Re4;
+        010:N=Mi4;
+        011:N=Fa4;
+        100:N=Sol4;
+        101:N=La4;
+        110:N=Si4;
+        111:N=Do5;
+        endcase        
+     if (divcounter == N - 1) 
         begin 
-           clk1=clk1
+           clk1=clk1;
           divcounter <= 0;
         end
   else 
@@ -17,4 +37,4 @@ always @(posedge clk_in)
       end 
 assign clk_out= clk1;
 
-nd module
+endmodule
