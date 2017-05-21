b6939f1cac5ed4aa68c9656dc96cd855f0e5ffbe
jaaguzmanro <jaaguzmanro@unal.edu.co>
2017-05-21 16:31:20 -0500

    Update divfreq.v

--- divfreq.v
+++ divfreq.v
@@ -1,33 +1,29 @@
-module divfreq(freq,chord)
+module divfreq(freq,chord,clk)
 
 output reg[7:0] freq;
 input reg [7:0] chord;
+input clk:
 
-wire [16:0] Do4 =	191113
-wire [16:0] Re4	= 170263
-wire [16:0] Mi4	= 151687
-wire [16:0] Fa4	= 143173
-wire [16:0] Sol4 = 127553
-wire [16:0] La4	= 113637
-wire [16:0] Si4	= 101239
-wire [16:0] Do5	= 95557
-
-
-
-
-
-reg [1:0] divcounter = 0;
+    wire [17:0] Do4 = 191113;
+    wire [17:0] Re4	= 170263;
+    wire [17:0] Mi4	= 151687;
+    wire [17:0] Fa4	= 143173;
+    wire [17:0] Sol4 = 127553;
+    wire [17:0] La4	= 113637;
+    wire [17:0] Si4	= 101239;
+    wire [17:0] Do5	= 95557
+    reg [7:0] divcounter0 = 0;
     
-//-- Contador módulo 3
 always @(posedge clk_in)
-  if (divcounter == 2) 
+    
+
+ if (divcounter == M - 1) 
     divcounter <= 0;
   else 
     divcounter <= divcounter + 1;
     
 //-- Sacar el bit mas significativo por clk_out
-assign clk_out = divcounter[1];
-    
+assign clk_out = divcounter[N-1];
 
 
 
