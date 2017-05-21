33c310825a534d3b717aeb5146ad486629274a37
jaaguzmanro <jaaguzmanro@unal.edu.co>
2017-05-21 18:41:02 -0500

    Update piano.v

--- piano.v
+++ piano.v
@@ -14,6 +14,25 @@ module piano( wave, t0,t1,t2,t3,t4,t5,t6,t7,clk);
   wire [7:0] wave1,wave2,wave3,wave4,wave5,wave6,wave7,wave0,sum1,sum2,sum3,sum4,sum5,sum6,sum7;
   wire ci0,ci1,ci2,ci3,ci4,ci5;
   
+  
+   divfreq note0(freq,chord,clk);
+            rom n0(freq[0],wave0);
+            rom n1(freq[1],wave1);
+            rom n2(freq[2],wave2);
+            rom n3(freq[3],wave3);
+            rom n4(freq[4],wave4);
+            rom n5(freq[5],wave5);
+            rom n6(freq[6],wave6);
+            rom n7(freq[7],wave7);
+            sumres suma1(sum1,wave0,wave1,ci0);//res,sig,a,b,ci,ctrl 
+            sumres suma2(sum2,wave2,sum1,ci0);
+            sumres suma3(sum3,wave3,sum2,ci0);
+            sumres suma4(sum4,wave4,sum3,ci0);
+            sumres suma5(sum5,wave5,sum4,ci0);
+            sumres suma6(sum6,wave6,sum5,ci0);
+            sumres suma7(sum7,wave7,sum6,ci0);      
+  
+  
   always @(posedge clk)
     begin
         case (STATE)
@@ -24,31 +43,16 @@ module piano( wave, t0,t1,t2,t3,t4,t5,t6,t7,clk);
           end  
         GET_FREQ:         
           begin
-            divfreq note0(freq,chord,clk);
+           
             STATE=READ;
           end 
         READ:
           begin
-            rom n0(freq[0],wave0);
-            rom n1(freq[1],wave1);
-            rom n2(freq[2],wave2);
-            rom n3(freq[3],wave3);
-            rom n4(freq[4],wave4);
-            rom n5(freq[5],wave5);
-            rom n6(freq[6],wave6);
-            rom n7(freq[7],wave7);
             STATE = SUM;
           end
           
         SUM:
           begin
-            sumres suma1(sum1,wave0,wave1,ci0);//res,sig,a,b,ci,ctrl 
-            sumres suma2(sum2,wave2,sum1,ci0);
-            sumres suma3(sum3,wave3,sum2,ci0);
-            sumres suma4(sum4,wave4,sum3,ci0);
-            sumres suma5(sum5,wave5,sum4,ci0);
-            sumres suma6(sum6,wave6,sum5,ci0);
-            sumres suma7(sum7,wave7,sum6,ci0);      
             STATE=PLAY;
           end
         PLAY:assign wave <= sum7;
