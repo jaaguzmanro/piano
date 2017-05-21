cf14e908145f899f2bd7fe10235e7e90d1613f82
jaaguzmanro <jaaguzmanro@unal.edu.co>
2017-05-21 17:24:05 -0500

    Update piano.v

--- piano.v
+++ piano.v
@@ -11,6 +11,7 @@ module piano( wave, t0,t1,t2,t3,t4,t5,t6,t7,clk);
   wire [3:0] PLAY= 3'b100;
   wire [7:0] Chord= 8'b00000000;
   wire [7:0] freq= 8'b00000000;
+  wire [7:0] wave1,wave2,wave3,wave4,wave5,wave6,wave7,wave0;
  
   
   always @(posedge clk)
@@ -28,7 +29,14 @@ module piano( wave, t0,t1,t2,t3,t4,t5,t6,t7,clk);
           end 
         READ:
           begin
-            
+            rom n0(freq[0],wave0);
+            rom n1(freq[1],wave1);
+            rom n2(freq[2],wave2);
+            rom n3(freq[3],wave3);
+            rom n4(freq[4],wave4);
+            rom n5(freq[5],wave5);
+            rom n6(freq[6],wave6);
+            rom n7(freq[7],wave7);
           end
         SUM:
         PLAY:
