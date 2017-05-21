4ec833e4285edf875842feac31062275b481f78e
jaaguzmanro <jaaguzmanro@unal.edu.co>
2017-05-21 16:42:54 -0500

    Update divfreq.v

--- divfreq.v
+++ divfreq.v
@@ -4,15 +4,10 @@ output reg[7:0] freq;
 input reg [7:0] chord;
 input clk:
 
-    wire [17:0] Do4 = 191113;
-    wire [17:0] Re4	= 170263;
-    wire [17:0] Mi4	= 151687;
-    wire [17:0] Fa4	= 143173;
-    wire [17:0] Sol4 = 127553;
-    wire [17:0] La4	= 113637;
-    wire [17:0] Si4	= 101239;
-    wire [17:0] Do5	= 95557
-    wire [7:0] divcounter0 = 0;
+    
+    
+    
+    
     
     
 
