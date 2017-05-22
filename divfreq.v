8fc98748dc30871d5704ef9387151e2dddc2a838
jaaguzmanro <jaaguzmanro@unal.edu.co>
2017-05-22 09:36:07 -0500

    Update divfreq.v

--- divfreq.v
+++ divfreq.v
@@ -1,53 +1,35 @@
-module divfreq(freq,chord,clk)
+module divfreq(freq,chord,clk);
 
-output reg[7:0] freq;
-input reg [7:0] chord;
+output wire[7:0] freq;
+input wire [7:0] chord;
 input clk;
 
-    wire [2:0] n0=3'b000;    
+    reg [2:0] n0=3'b000;    
     wire [2:0] n1=3'b001;    
     wire [2:0] n2=3'b010;    
     wire [2:0] n3=3'b011;    
     wire [2:0] n4=3'b100;    
     wire [2:0] n5=3'b101;    
     wire [2:0] n6=3'b110;    
-    wire [2:0] n8=3'b111;    
-    
-    assign n0=n0*chord[0];
-    assign n1=n1*chord[1];
-    assign n2=n2*chord[2];
-    assign n3=n3*chord[3];
-    assign n4=n4*chord[4];
-    assign n5=n5*chord[5];
-    assign n6=n6*chord[6];
-    assign n7=n7*chord[7];
-    
-    
-    div div0(freq[0],n0,clk);
-    div div1(freq[1],n1,clk);
-    div div2(freq[2],n2,clk);
-    div div3(freq[3],n3,clk);
-    div div4(freq[4],n4,clk);
-    div div5(freq[5],n5,clk);
-    div div6(freq[6],n6,clk);
-    div div7(freq[7],n7,clk);
+    wire [2:0] n7=3'b111;    
+    wire[2:0] m0,m1,m2,m3,m4,m5,m6,m7;
+    assign m0=n0*chord[0];
+    assign m1=n1*chord[1];
+    assign m2=n2*chord[2];
+    assign m3=n3*chord[3];
+    assign m4=n4*chord[4];
+    assign m5=n5*chord[5];
+    assign m6=n6*chord[6];
+    assign m7=n7*chord[7];
     
     
-
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
+    div div0(freq[0],m0,clk);
+    div div1(freq[1],m1,clk);
+    div div2(freq[2],m2,clk);
+    div div3(freq[3],m3,clk);
+    div div4(freq[4],m4,clk);
+    div div5(freq[5],m5,clk);
+    div div6(freq[6],m6,clk);
+    div div7(freq[7],m7,clk);
+      
 endmodule
