d887c1353f86657566b0d38fcc46097b0d6f23ab
jaaguzmanro <jaaguzmanro@unal.edu.co>
2017-05-22 09:31:02 -0500

    Update div.v

--- div.v
+++ div.v
@@ -1,40 +1,42 @@
-module div(clk_out,scaler,clk_in)
-    input reg [2:0]scaler
+module div(clk_out,scaler,clk_in);
+input wire [2:0]scaler;
 input clk_in;
 output clk_out;
-wire clk1;
-    wire [17:0] Do4 = 191113;
-    wire [17:0] Re4	= 170263;
-    wire [17:0] Mi4	= 151687;
-    wire [17:0] Fa4	= 143173;
-    wire [17:0] Sol4 = 127553;
-    wire [17:0] La4	= 113637;
-    wire [17:0] Si4	= 101239;
-    wire [17:0] Do5	= 95557;
-    wire [17:0] N	= 0;
+reg clk1=0;
+    reg [17:0] Do4 = 191113;
+    reg [17:0] Re4	= 170263;
+    reg [17:0] Mi4	= 151687;
+    reg [17:0] Fa4	= 143173;
+    reg [17:0] Sol4 = 127553;
+    reg [17:0] La4	= 113637;
+    reg [17:0] Si4	= 101239;
+    reg[17:0] Do5	= 95557;
+    reg [17:0] N	;
+	 reg [17:0] divcounter;
     
 always @(posedge clk_in)
-    
+ begin   
     case (scaler)
-        000:N=Do4;
-        001:N=Re4;
-        010:N=Mi4;
-        011:N=Fa4;
-        100:N=Sol4;
-        101:N=La4;
-        110:N=Si4;
-        111:N=Do5;
-        endcase        
+        000:N<=Do4;
+        001:N<=Re4;
+        010:N<=Mi4;
+        011:N<=Fa4;
+        100:N<=Sol4;
+        101:N<=La4;
+        110:N<=Si4;
+        111:N<=Do5;
+        endcase   
+
      if (divcounter == N - 1) 
         begin 
-           clk1=clk1;
+          clk1  <= ~clk1;
           divcounter <= 0;
         end
   else 
-      begin
-          divcounter <= divcounter + 1;
-          clk1=~clk1;
-      end 
-assign clk_out= clk1;
+             divcounter <= divcounter + 1;
+          
+end
+
+assign clk_out = clk1;
 
 endmodule
